547f9c11f57c2cb56029a0de5b3a9779049514ba
jy0813 <busbug3@gmail.com>
2022-10-02 15:28:20 +0900

    feat: ProductList 생성

---
+++ components/ProductList.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <div>
+      {{ products }}
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  async asyncData() {
+    const response = await axios.get('http://localhost:8000/products')
+    const products = response.data
+    return { products }
+  },
+}
+</script>
+
+<style scoped></style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,13 @@
 <template>
   <div>
     <h1>메인</h1>
-    <div>{{ products }}</div>
+    <ProductList></ProductList>
   </div>
 </template>
 
 <script>
-import axios from 'axios'
+import ProductList from '~/components/ProductList.vue'
 export default {
-  async asyncData() {
-    const response = await axios.get('http://localhost:8000/products')
-    const products = response.data
-    return { products }
-  },
+  components: { ProductList },
 }
 </script>
